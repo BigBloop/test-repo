606d69d8c5be8a3e74f859a03eeced68a5567ab8
BigBloop <ansomofficial@gmail.com>
2022-01-28 23:16:35 +0800

    removed the for loop in main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include "feature1.cpp"
 int main()
 {
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << "Marco is the best! Reapeats: " << i+1 << '\n';
-    }
     printGreet();
     return 0;
 }
